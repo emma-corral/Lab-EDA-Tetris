7a24d75e3a7208af0f00d1796ed9c9be33dc3606
u252297 <emma.corral01@estudiant.upf.edu>
2025-01-30 13:14:54 +0100

    Update session.c

--- L1_code_v1.2 (1)/student/session.c
+++ L1_code_v1.2 (1)/student/session.c
@@ -2,12 +2,12 @@
 
 void restart_session_game_state(Session *session){
     // ToDo in LAB 1
-    init_game_state(&session->current_game_state);
+    init_game_state(&session->current_game_state);   //It call to the initialization of the game state(to continue with your previues game)
 }
 
 void init_session(Session *session){
-    session->best_score = 0;
-    restart_session_game_state(session);
+    session->best_score = 0;              // It reset the best score to 0
+    restart_session_game_state(session);  // Restart te game state ot the session
     // ToDo in LAB 1
 }
 
