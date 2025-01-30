08caad801f9f4e363a19f7be0bb9751723a19375
u252297 <emma.corral01@estudiant.upf.edu>
2025-01-30 12:42:33 +0100

    Update session.c

--- L1_code_v1.2 (1)/student/session.c
+++ L1_code_v1.2 (1)/student/session.c
@@ -2,9 +2,12 @@
 
 void restart_session_game_state(Session *session){
     // ToDo in LAB 1
+    init_game_state(&session->current_game_state);
 }
 
 void init_session(Session *session){
+    session->best_score = 0;
+    restart_session_game_state(session);
     // ToDo in LAB 1
 }
 
