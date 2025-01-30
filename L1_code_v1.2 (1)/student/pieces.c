8e59a0e4edb628e173ee2d3da6b47543792359dd
u252297 <emma.corral01@estudiant.upf.edu>
2025-01-30 13:06:45 +0100

    Update pieces.c

--- L1_code_v1.2 (1)/student/pieces.c
+++ L1_code_v1.2 (1)/student/pieces.c
@@ -4,7 +4,7 @@ void init_piece(Piece *p){
     char FondoFicha[4][4];
 	for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            FondoFicha[i][j] = '.';
+            FondoFicha[i][j] = '.';         //It creates the board(background) of the piece (4x4) with '.'
         }
     };    
 }
@@ -13,7 +13,7 @@ void print_piece(Piece p){
     printf("Name: %c; Size: %dx%d\n", p.name, p.rows, p.cols);
     for(int i=0; i<PIECE_SIZE; ++i){
         for(int j=0; j<PIECE_SIZE; ++j){
-            printf("%c", p.board[i][j]);
+            printf("%c", p.board[i][j]);                       //It prints the piece
         }
         printf("\n");
     }
@@ -26,7 +26,7 @@ void rotate_clockwise(Piece *p){
 	//Define an empty temporary variable to store the rotated piece using init_piece(...).
 	for(int c=0; c< PIECE_SIZE; ++c){
 		for(int r=0; r< PIECE_SIZE; ++r){
-			temp.board[c][p->rows-1-r] = p->board[r][c];
+			temp.board[c][p->rows-1-r] = p->board[r][c];     //it iterates between all columns and rows (concatenated for loops) in order to rotate it
 		}
 	}
 	*p = temp;
@@ -39,7 +39,7 @@ void rotate_counter_clockwise(Piece *p){
 	//Define an empty temporary variable to store the rotated piece using init_piece(...).
 	for( int c=0;c< PIECE_SIZE; ++c){
 		for(int r=0;r< PIECE_SIZE; ++r){
-			temp.board[r][c] = p->board[p->rows-1-c][r];
+			temp.board[r][c] = p->board[p->rows-1-c][r];     //it iterates between all columns and rows (concatenated for loops) in order to rotate it
 		}
 	}
 	*p = temp;
@@ -47,9 +47,9 @@ void rotate_counter_clockwise(Piece *p){
 
 Piece make_O(){
 	Piece piece;
-	init_piece(&piece);
+	init_piece(&piece);                             // It draw the board where the piece is going to appear
 	piece.name= 'O'; piece.rows = 2; piece.cols= 2;
-	piece.board[0][0] = '#';
+	piece.board[0][0] = '#';                        // Sustituted the '.' whith # to draw the pice
 	piece.board[0][1]= '#';
 	piece.board[1][0] ='#';
 	piece.board[1][1] = '#';
