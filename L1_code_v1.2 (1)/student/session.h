04a6c5aab225ee0dbdb3cc411cb0cd0f523663db
u252297 <emma.corral01@estudiant.upf.edu>
2025-01-30 12:41:50 +0100

    Update session.h

--- L1_code_v1.2 (1)/student/session.h
+++ L1_code_v1.2 (1)/student/session.h
@@ -1,14 +1,15 @@
 #ifndef _SESSION_H_
-#define _SESSION_H_ 
+#define _SESSION_H_
 
 #include "game.h"
 
 /// LAB 1 - data structure
-typedef struct{
-    // ToDo in LAB 1 - define data structure
+typedef struct {
+    int best_score;               // Best score achieved across all games
+    GameState current_game_state; // Current game state, including the board, score, and current piece
 } Session;
 
-/// LAB 1 - functions 
+/// LAB 1 - functions
 void restart_session_game_state(Session *session);
 void init_session(Session *session);
 
