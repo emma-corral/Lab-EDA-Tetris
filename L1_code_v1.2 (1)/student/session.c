7637025869995e8935f65bf08f00f19de7195fb7
u251986 <nahia.azpilicueta01@estudiant.upf.edu>
2025-02-13 12:10:41 +0100

    Update session.c

--- L1_code_v1.2 (1)/student/session.c
+++ L1_code_v1.2 (1)/student/session.c
@@ -1,13 +1,9 @@
-#include "session.h"
-
 void restart_session_game_state(Session *session){
-    // ToDo in LAB 1
-    init_game_state(&session->current_game_state);   //It call to the initialization of the game state(to continue with your previues game)
+    restart_game_state(session);
 }
 
 void init_session(Session *session){
-    session->best_score = 0;              // It reset the best score to 0
-    restart_session_game_state(session);  // Restart te game state ot the session
-    // ToDo in LAB 1
+    session->best_score = 0;
+    init_game_state(&session->current_game_state);
 }
 
