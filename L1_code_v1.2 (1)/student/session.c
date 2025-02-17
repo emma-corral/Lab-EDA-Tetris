ff7de0aeb51863289cf2c814d48036dbb178a20d
u252297 <emma.corral01@estudiant.upf.edu>
2025-02-17 11:39:21 +0100

    Update session.c

--- L1_code_v1.2 (1)/student/session.c
+++ L1_code_v1.2 (1)/student/session.c
@@ -1,13 +1,12 @@
 #include "session.h"
 
-void restart_session_game_state(Session *session){
+void restart_session_game_state(Session *session){   //we refactorede it
     // ToDo in LAB 1
     restart_game_state(&session->current_game_state);   //It call to the initialization of the game state(to continue with your previues game)
 }
 
-void init_session(Session *session){
+void init_session(Session *session){  //we refactored it
     session->best_score = 0;              // It reset the best score to 0
     init_game_state(&session->current_game_state);  // Restart te game state ot the session
     // ToDo in LAB 1
 }
-
